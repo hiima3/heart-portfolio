0ad17a6b91569bfdcf329523360ed1e43aac1fbc
HiImCovid19 <84524848+HiImCovid19@users.noreply.github.com>
2025-08-08 13:35:04 +0900

    Update top.js
    スクロール時のハート位置変更

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var mainCopies = document.querySelectorAll(".main_copy");
-    var heartElement = document.querySelector("#heart_color");
-    var heartHeight = heartElement.offsetHeight + 300;
+document.addEventListener("DOMContentLoaded", function () {
+  var mainCopies = document.querySelectorAll(".main_copy");
+  var heartElement = document.querySelector("#heart_color");
+  var heartHeight = heartElement.offsetHeight + 150;
 
-    window.addEventListener("scroll", function() {
-            var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  window.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-            mainCopies.forEach(function(mainCopy) {
-            if (scrollPosition > heartHeight) {
-            mainCopy.classList.add("is-absolute");
-            } else {
-            mainCopy.classList.remove("is-absolute");
-            }
-        });
+    mainCopies.forEach(function (mainCopy) {
+      if (scrollPosition > heartHeight) {
+        mainCopy.classList.add("is-absolute");
+      } else {
+        mainCopy.classList.remove("is-absolute");
+      }
     });
+  });
 });
-
